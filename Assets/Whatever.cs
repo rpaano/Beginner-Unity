bd891f28db529d6af38756a942473186ab43226d
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-06-14 19:39:49 +0800

    example variables added

--- Assets/Whatever.cs
+++ Assets/Whatever.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Whatever : MonoBehaviour
 {
+    int score = 0;
+
+    float meters = 1.3345678f;
+
+    string name = "Awesome Hero";
+
+    bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
