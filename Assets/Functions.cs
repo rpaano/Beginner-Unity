d17e6f18bac696d831d6a60cd0724139966c984c
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-06-14 19:49:21 +0800

    Print Function

--- Assets/Functions.cs
+++ Assets/Functions.cs
@@ -7,12 +7,17 @@ public class Functions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Print();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Print()
+    {
+        Debug.Log("Hello");
     }
 }
